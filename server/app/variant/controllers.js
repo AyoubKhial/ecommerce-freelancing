84dd88b76a248e8d3ae39b6f250ddcedc0a5af1f
Ayoub KHIAL <akhial94@outlook.com>
2021-01-23 23:39:06 +0100

    Add variant's CRUD functionalities

---
+++ server/app/variant/controllers.js
@@ -0,0 +1,35 @@
+const database = require('../../database');
+
+const Variant = database.variants;
+const Op = database.Sequelize.Op;
+
+const create = async (req, res) => {
+    const variant = await Variant.create(req.body);
+    res.status(201).send(variant);
+};
+
+const find = async (req, res) => {
+    const variants = await Variant.findAll({});
+    res.status(200).send(variants);
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+    const variant = await Variant.findByPk(id);
+    res.status(200).send(variant);
+};
+
+const updateById = async (req, res) => {
+    const id = req.params.id;
+    const variant = req.body;
+    await Variant.update(variant, { where: { id }});
+    res.status(201).send({ message: 'updated Successfully' });
+};
+
+const deleteById = async (req, res) => {
+    const id = req.params.id;
+    await Variant.destroy({ where: { id }});
+    res.status(201).send({ message: 'deleted Successfully' });
+};
+
+module.exports = { create, find, findById, updateById, deleteById };
--- server/app/variant/routes.js
+++ server/app/variant/routes.js
@@ -1,5 +1,9 @@
 const controller = require('./controllers');
 
 module.exports = app => {
-    
+    app.post("/api/variants", controller.create);
+    app.get("/api/variants", controller.find);
+    app.get("/api/variants/:id", controller.findById);
+    app.put("/api/variants/:id", controller.updateById);
+    app.delete("/api/variants/:id", controller.deleteById);
 };
