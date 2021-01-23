17fc515ef39508c7c2f46a00ccb761b1a699a825
Ayoub KHIAL <akhial94@outlook.com>
2021-01-24 00:14:59 +0100

    Add product crud operations

---
+++ server/app/product/controllers.js
@@ -0,0 +1,35 @@
+const database = require('../../database');
+
+const Product = database.products;
+const Op = database.Sequelize.Op;
+
+const create = async (req, res) => {
+    const product = await Product.create(req.body);
+    res.status(201).send(product);
+};
+
+const find = async (req, res) => {
+    const products = await Product.findAll({});
+    res.status(200).send(products);
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByPk(id);
+    res.status(200).send(product);
+};
+
+const updateById = async (req, res) => {
+    const id = req.params.id;
+    const product = req.body;
+    await Product.update(product, { where: { id }});
+    res.status(201).send({ message: 'updated Successfully' });
+};
+
+const deleteById = async (req, res) => {
+    const id = req.params.id;
+    await Product.destroy({ where: { id }});
+    res.status(201).send({ message: 'deleted Successfully' });
+};
+
+module.exports = { create, find, findById, updateById, deleteById };
--- server/app/product/routes.js
+++ server/app/product/routes.js
@@ -1,3 +1,9 @@
 const controller = require('./controllers');
 
-module.exports = app => {};
+module.exports = app => {
+    app.post("/api/products", controller.create);
+    app.get("/api/products", controller.find);
+    app.get("/api/products/:id", controller.findById);
+    app.put("/api/products/:id", controller.updateById);
+    app.delete("/api/products/:id", controller.deleteById);
+};
