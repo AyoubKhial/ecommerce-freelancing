bc9515068b472d8c385d5e6f18c3e8603ac0fd97
Ayoub KHIAL <akhial94@outlook.com>
2021-01-28 00:00:08 +0100

    Fix auth service

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const API_URL = `${environment.server}api/users/`;
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
-
+export interface IUser {
+	firstName?: string;
+	lastName?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
-      username,
-      password
-    }, httpOptions);
-  }
+	constructor(private http: HttpClient) { }
 
-  register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
-      username,
-      email,
-      password
-    }, httpOptions);
-  }
+	register(user: IUser): Observable<IUser> {
+		return this.http.post<IUser>(API_URL + 'register', user);
+	}
+
+	login(user: IUser): Observable<any> {
+		return this.http.post<any>(API_URL + 'login', user);
+	}
 }
